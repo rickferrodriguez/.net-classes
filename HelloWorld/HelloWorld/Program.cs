6b4c8c4c0a0e592ef2e225eb0ce2683b05512472
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-12-01 00:51:37 -0500

    area de un rectángulo

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+var ladoA = 0d;
+var ladoB = 0d;
+var resultado = 0d;
+
+Console.WriteLine("Calcula el valor del area de un rectangulo");
+
+Console.WriteLine("por favor ingrese el valor del lado A");
+ladoA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Por favor ingrese el valor del lado B");
+ladoB = Convert.ToDouble(Console.ReadLine());
+
+resultado = ladoA * ladoB;
+Console.WriteLine("El valor del area del rectángulo es: " + resultado);
