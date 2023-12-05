22f79c914500c59f10eda799daf4ce29b2dd5315
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-12-05 12:25:55 -0500

    Final de clase de los strings

--- Strings/Strings/Program.cs
+++ Strings/Strings/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hola richard rodriguez");
+int height = 175;
+int age = 25;
+string name = "Richard Rodriguez";
+string information = "Nació en Pasto nariño, es estudiante de ingeniería de sistemas y c#";
+var hobby = "deportista";
+
+var identificationCard =
+  $"La información de {name} es la siguiente: \n Su edad es {age} \n Esta persona mide {height} \n y su información general es: {information} \n Sus hobbies son: {hobby}";
+
+Console.WriteLine(identificationCard);
