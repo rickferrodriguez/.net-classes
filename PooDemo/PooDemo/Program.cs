90204bca4e3f92a9226637cf53e7985fd2f1d828
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-12-10 18:36:41 -0500

    a new custom class: Artist

---
+++ PooDemo/PooDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+// with this you can create a varible that has the type of the Class in this example
+Pointer platziPointer = new Pointer();
+Pointer platziPointer2 = new Pointer();
+
+// you can use the properties from the Class and assign a value
+platziPointer.Color = "Black";
+platziPointer2.Color = "Black";
+
+// a class is a mold that you can reuse
+class Pointer
+{
+  // this down below are called properties
+  public string Size;
+  public string Color;
+  public bool HasSticker;
+  public short NumberOfButtons;
+}
+
+// the idea when you creat a class is that you have an object and try to guess what could have all of the same objects in common
+class Keyboard
+{
+  public string Size;
+  public string Color;
+  public string Layout;
+  public bool HasLeds;
+  public bool HasCustomKeyCaps;
+}
+
+class Artist
+{
+  public string ArtisticName;
+  public string RealName;
+  public DateTime DateOfBirth;
+  public List<string> Albums;
+  public string Genre;
+  public bool ItsABand;
+}
+ 
+ 
