8d5b1f0503478a173428e6e97f7c518b2743ea2d
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-12-11 21:58:29 -0500

    methods in c#

--- SuperHeroesApp/SuperHeroesApp/Program.cs
+++ SuperHeroesApp/SuperHeroesApp/Program.cs
@@ -1,16 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
-
-var flyPower = new SuperPower();
+﻿var flyPower = new SuperPower();
 flyPower.Name = "Fly Power";
 flyPower.Description = "The hability to levitate in the air";
 flyPower.Level = LevelOfPower.Level1;
 
 var superStrong = new SuperPower();
-flyPower.Name = "Super Strong";
-flyPower.Description = "When a person has x10 strength than a Human";
-flyPower.Level = LevelOfPower.Level2;
+superStrong.Name = "Super Strong";
+superStrong.Description = "When a person has x10 strength than a Human";
+superStrong.Level = LevelOfPower.Level2;
 
 var bulletResistance = new SuperPower();
 bulletResistance.Name = "Bullet Resistance";
@@ -24,7 +20,9 @@ var hulk = new SuperHero();
 List<SuperPower> superManSuperPowers = new List<SuperPower>();
 superManSuperPowers.Add(flyPower);
 superManSuperPowers.Add(superStrong);
+superManSuperPowers.Add(bulletResistance);
 
+superman.Id = 1;
 superman.Name = "SuperMan";
 superman.RealName = "Clark Kent";
 superman.City = "Small Ville";
@@ -35,29 +33,56 @@ List<SuperPower> hulkSuperPowers = new List<SuperPower>();
 hulkSuperPowers.Add(superStrong);
 hulkSuperPowers.Add(bulletResistance);
 
+hulk.Id = 2;
 hulk.Name = "hulk";
 hulk.RealName = "Bruce Banner";
 hulk.City = "New York";
 hulk.CanFly = false;
 hulk.Powers = hulkSuperPowers;
 
+Console.WriteLine(Convert.ToString(superman.UseSuperPowers()));
 
 
 class SuperHero
 {
+  public int Id;
   public string Name;
   public string RealName;
   public string City;
   public List<SuperPower> Powers;
   public bool CanFly;
+
+  public SuperHero()
+  {
+    Id = 1;
+    // we initialize a new list of SuperPowers with zero values
+    Powers = new List<SuperPower>();
+    CanFly = false;
+  }
+
+  public string UseSuperPowers()
+  {
+    string message = $"{Name} have this list of super powers: ";
+    foreach (var power in Powers)
+    {
+      message = message + $"{power.Name}, ";
+    }
+
+    return message;
+  }
 }
 
 // Creat a class only for the type SuperPowers
 class SuperPower
 {
-  public string Name;
+  public string? Name;
   public string Description;
   public LevelOfPower Level;
+
+  public SuperPower()
+  {
+    Level = LevelOfPower.Level1;
+  }
 }
 
 // with 'enum' we can create a new value type only for the Level in the class SuperPower
